e519d8141ad065bf85a3be00b0c4c1663d272245
shanelo7a <aabdelm1@umd.edu>
2017-03-27 23:45:22 -0400

    Update RRTF.m

--- RRTF.m
+++ RRTF.m
@@ -1,24 +1,17 @@
 %%declare variables
-clear All
-close All
-xMin=0;
-yMin=0;
-xMax=100;
-yMax=100;
-xStart=0; 
-yStart=0;
-xGoal=80;
-yGoal=90;
+clear All close All
+xMin=0;     yMin=0;
+xMax=100;   yMax=100;
+xStart=0;   yStart=0;
+xGoal=80;   yGoal=90;
 rGoal=20;
 k=1000;
 epsl=5;
 V=cell(1,5);
 xCurrent=0;
 yCurrent=0;
-V{1,1}(1)=0;
-V{1,2}(1)=0;
-V{1,3}(1)=0;
-V{1,4}(1)=0;
+V{1,1}(1)=0; V{1,2}(1)=0;
+V{1,3}(1)=0; V{1,4}(1)=0;
 obsArray = csvread('obstacles.txt');
 
 figure(1); hold on; grid on;
