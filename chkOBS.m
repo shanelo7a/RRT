d2308d182ab1b1538ed8033586407c208fbf39c2
shanelo7a <aabdelm1@umd.edu>
2017-04-02 14:12:50 -0400

    Update chkOBS.m

--- chkOBS.m
+++ chkOBS.m
@@ -3,9 +3,9 @@ function [OBSTCL] = chkOBS(x,y,obsAry)
 for i=1:length(obsAry)
     chk=euclidean(x,y,obsAry(i,1),obsAry(i,2));
     if chk <= obsAry(i,3)
-        OBSTCL = 0;
-    else
         OBSTCL = 1;
+    else
+        OBSTCL = 0;
     end
 end
 
